ffab498daadbc7d1eaf6c96062fe4aa89070f439
Aditya Praveen <adityapraveen18@gmail.com>
2023-08-04 22:02:55 +0530

    C

--- RemovingHighValues.py
+++ RemovingHighValues.py
@@ -2,3 +2,6 @@ data = [1, 3, 34, 54, 344, 352, 544, 233, 554, 43, 23]
 
 min_valid = 100
 max_valid = 300
+ 
+
+print("Hello world")
---
+++ lists_intro.py
@@ -0,0 +1,7 @@
+computer_parts = ["keyboard", "mouse", "screen", "RAM"]
+
+for part in computer_parts:
+    print(part)
+
+print()
+print(computer_parts[2])
